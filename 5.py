e1e4eaaf047ec51b9ed4be5b298d1f0331f6ba69
KASHISH SHARMA <95432071+Kashish9086@users.noreply.github.com>
2023-12-05 12:19:18 +0530

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,12 @@
+# Input the two integers
+num1 = int(input())
+num2 = int(input())
+
+# Print the sum of the two numbers
+print(num1 + num2)
+
+# Print the difference of the two numbers (first - second)
+print(num1 - num2)
+
+# Print the product of the two numbers
+print(num1 * num2)
